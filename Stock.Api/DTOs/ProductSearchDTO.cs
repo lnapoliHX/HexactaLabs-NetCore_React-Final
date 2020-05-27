4cf3aa25e7ef04b656c2b860ad78b014d384800a
Juan Bernal <jbernal@hexacta.com>
2020-05-27 11:49:03 -0300

    Fix front and back issues

--- Stock.Api/DTOs/ProductSearchDTO.cs
+++
@@ -1,9 +0,0 @@
-namespace Stock.Api.DTOs
-{
-    public class ProductSearchDTO
-    { 
-        public string Name { get; set; }
-        public string Brand { get; set; }
-        public ActionDto Condition { get; set; } = ActionDto.AND;
-    }
-}
