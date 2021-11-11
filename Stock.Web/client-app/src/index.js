62c7488ba70e81f45629e2bb80b621fc6b1fc84b
Camilo Villate <camilovillate@192.168.2.110>
2021-11-11 17:05:39 -0500

    Change some colors and add icons

--- Stock.Web/client-app/src/index.js
+++ Stock.Web/client-app/src/index.js
@@ -42,10 +42,10 @@ const menu_factory = (title, to, icon, children = []) => ({
 });
 
 const menu = [
-  menu_factory("Proveedor", "/provider", "algun-icon"),
-  menu_factory("Producto", "/product", "algun-icon"),
-  menu_factory("Categorías", "/product-type", "algun-icon"),
-  menu_factory("Tiendas", "/store", "algun-icon")
+  menu_factory("Proveedor", "/provider", "img_provider"),
+  menu_factory("Producto", "/product", "img_provider"),
+  menu_factory("Categorías", "/product-type", "img_provider"),
+  menu_factory("Tiendas", "/store", "img_store")
 ];
 
 ReactDOM.render(
